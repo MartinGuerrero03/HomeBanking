ad2b7952f3e3c6dd1d6076f8b249bcc4f7131873
Martin <martinnguerrero99@gmail.com>
2023-07-25 12:18:42 -0300

    Agregar archivos de proyecto.

---
+++ Models/DbInitializer.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+
+namespace HomeBanking.Models
+{
+    public class DbInitializer
+    {
+        public static void DbInitialize(HomeBankingContext context) 
+        {
+            if (!context.Clients.Any())
+            {
+                var clients = new Client[] {
+                new Client
+                {
+                    Email = "martin@gmail.com",
+                    FirstName = "Martin",
+                    LastName = "Guerrero",
+                    Password = "12345"
+                }};
+
+                foreach (Client client in clients) 
+                {
+                    context.Clients.Add(client);
+                }
+
+                //guardamos
+                context.SaveChanges();
+            }
+        
+        }
+    }
+}
---
+++ Models/HomeBankingContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Threading.Tasks;
+
+namespace HomeBanking.Models
+{
+    
+    public class HomeBankingContext : DbContext
+    {
+        public HomeBankingContext(DbContextOptions<HomeBankingContext> options) : base(options) { }
+
+        //dbsets
+        public DbSet<Client> Clients { get; set; }
+    }
+    
+
+}
